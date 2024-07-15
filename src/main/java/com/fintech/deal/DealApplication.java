e3c632341a31478ba5aee2995b0eed2561865a91
1dlvb <matushkin-2017@mail.ru>
2024-07-15 23:29:38 +0500

    feat: implement dotenv

--- src/main/java/com/fintech/deal/DealApplication.java
+++ src/main/java/com/fintech/deal/DealApplication.java
@@ -1,13 +1,19 @@
 package com.fintech.deal;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
 public class DealApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(DealApplication.class, args);
+
     }
 
 }
