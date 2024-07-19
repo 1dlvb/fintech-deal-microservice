7216bf03a6613758a677936f30241b6934e34a12
1dlvb <matushkin-2017@mail.ru>
2024-07-19 13:44:10 +0500

    feat: add new custom methods to ContractorRepository

--- src/main/java/com/fintech/deal/repository/ContractorRepository.java
+++ src/main/java/com/fintech/deal/repository/ContractorRepository.java
@@ -1,5 +1,6 @@
 package com.fintech.deal.repository;
 
+import com.fintech.deal.model.Deal;
 import com.fintech.deal.model.DealContractor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -15,7 +17,16 @@ public interface ContractorRepository extends JpaRepository<DealContractor, UUID
     @Query("SELECT COUNT(c) > 0 FROM DealContractor c WHERE c.main = true AND c.deal.id = :dealId")
     boolean existsByDealIdAndMainTrue(@Param("dealId") UUID dealId);
 
-    @Query("SELECT c FROM DealContractor c WHERE c.deal.id = :dealId")
+    @Query("SELECT c FROM DealContractor c WHERE c.deal.id = :dealId AND c.isActive = true")
     List<DealContractor> findAllByDealId(@Param("dealId") UUID dealId);
+
+    @Query("SELECT d FROM Deal d JOIN d.dealContractors c WHERE c.contractorId = :contractorId AND d.isActive = true")
+    List<Deal> findAllDealsByContractorId(@Param("contractorId") String contractorId);
+
+    @Query("SELECT COUNT(d) FROM Deal d JOIN d.dealContractors c WHERE c.contractorId = :contractorId AND d.status.id = 'ACTIVE' AND d.isActive = true")
+    Integer countDealsWithStatusActiveByContractorId(@Param("contractorId") String contractorId);
+
+    @Query("SELECT c FROM DealContractor c WHERE c.deal.id = :dealId AND c.contractorId = :contractorId")
+    Optional<DealContractor> findByDealIdAndContractorId(@Param("dealId") UUID dealId, @Param("contractorId") String contractorId);
 
 }
