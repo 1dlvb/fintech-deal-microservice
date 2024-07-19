62e3c69f37022d95e06bc515158059d8b1cc5777
1dlvb <matushkin-2017@mail.ru>
2024-07-19 19:32:14 +0500

    refactor: move url to a properties variable and change return value of updateActiveMainBorrower

--- src/main/java/com/fintech/deal/feign/ContractorFeignClient.java
+++ src/main/java/com/fintech/deal/feign/ContractorFeignClient.java
@@ -3,13 +3,14 @@ package com.fintech.deal.feign;
 import com.fintech.deal.dto.MainBorrowerDTO;
 import com.fintech.deal.feign.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "contractor", url = "localhost:8080", configuration = FeignConfig.class)
+@FeignClient(name = "contractor", url = "${contractor.service.url}", configuration = FeignConfig.class)
 public interface ContractorFeignClient {
 
     @PatchMapping("/contractor/main-borrower")
-    String updateActiveMainBorrower(@RequestBody MainBorrowerDTO dto);
+    ResponseEntity<Void> updateActiveMainBorrower(@RequestBody MainBorrowerDTO dto);
 
 }
