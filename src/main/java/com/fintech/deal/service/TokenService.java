d134de4630255388c034bed5bfb149af016b3f33
1dlvb <matushkin-2017@mail.ru>
2024-08-02 08:57:17 +0500

    feat: add Service transfer token to the interceptor

---
+++ src/main/java/com/fintech/deal/service/TokenService.java
@@ -0,0 +1,14 @@
+package com.fintech.deal.service;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Setter
+@Service
+public class TokenService {
+
+    private String token;
+
+}
