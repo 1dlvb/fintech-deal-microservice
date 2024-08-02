a9f2755ca339a1235941c46494fb4227f02e3c04
1dlvb <matushkin-2017@mail.ru>
2024-08-02 08:58:24 +0500

    feat: add sending jwt with feign request functionality

--- src/main/java/com/fintech/deal/feign/config/FeignConfig.java
+++ src/main/java/com/fintech/deal/feign/config/FeignConfig.java
@@ -1,16 +1,18 @@
 package com.fintech.deal.feign.config;
 
+import com.fintech.deal.interceptor.BearerTokenRequestInterceptor;
+import com.fintech.deal.service.TokenService;
 import feign.Client;
 import feign.codec.Encoder;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.beans.factory.ObjectFactory;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 
 /**
  * Configuration class for Feign clients.
@@ -42,6 +44,15 @@ public class FeignConfig {
     public Client feignClient() {
         CloseableHttpClient httpClient = HttpClients.custom().build();
         return new feign.httpclient.ApacheHttpClient(httpClient);
+    }
+
+    /**
+     * Provides a JWT request interceptor bean.
+     * @return the JWT request interceptor
+     */
+    @Bean
+    public BearerTokenRequestInterceptor jwtRequestInterceptor(TokenService tokenService) {
+        return new BearerTokenRequestInterceptor(tokenService);
     }
 
 }
---
+++ src/main/java/com/fintech/deal/interceptor/BearerTokenRequestInterceptor.java
@@ -0,0 +1,27 @@
+package com.fintech.deal.interceptor;
+
+import com.fintech.deal.service.TokenService;
+import com.google.common.net.HttpHeaders;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+@RequiredArgsConstructor
+public class BearerTokenRequestInterceptor implements RequestInterceptor {
+
+    private final TokenService tokenService;
+
+    @Override
+    public void apply(RequestTemplate template) {
+        final String authorization = HttpHeaders.AUTHORIZATION;
+        if (tokenService.getToken() != null) {
+                template.header(authorization);
+                template.header(authorization, "Bearer " + tokenService.getToken());
+        }
+    }
+
+}
