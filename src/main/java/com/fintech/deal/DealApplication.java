eae1a8ad570f2ec8486c112844cc231df7c8986d
1dlvb <matushkin-2017@mail.ru>
2024-08-16 13:53:51 +0500

    feat: add queue declaration

--- src/main/java/com/fintech/deal/DealApplication.java
+++ src/main/java/com/fintech/deal/DealApplication.java
@@ -1,15 +1,29 @@
 package com.fintech.deal;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
-@SpringBootApplication
-@EnableFeignClients
 @EnableScheduling
+@SpringBootApplication
 public class DealApplication {
+
+    private final RabbitAdmin rabbitAdmin;
+    private final Queue queue;
+
+    public DealApplication(RabbitAdmin rabbitAdmin, Queue queue) {
+        this.rabbitAdmin = rabbitAdmin;
+        this.queue = queue;
+    }
+
+    @PostConstruct
+    public void declareQueue() {
+        rabbitAdmin.declareQueue(queue);
+    }
 
     public static void main(String[] args) {
 
@@ -18,3 +32,4 @@ public class DealApplication {
     }
 
 }
+
